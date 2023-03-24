8bb9c6eccca2745032fa797684b3b1453214642f
[vivek kumar] <[vivekmisha140@gmail.com]>
2023-03-24 14:04:49 +0530

    set first name validation using js

--- basic.php
+++ basic.php
@@ -5,7 +5,8 @@
         <div class="col-md-4">
         <div class="form-group">
     <label for="firstname">FirstName:</label>
-    <input type="email" class="form-input" id="firstname" aria-describedby="emailHelp" placeholder="Enter FirstName">
+    <input type="email" class="form-input" id="firstname" aria-describedby="emailHelp" placeholder="Enter FirstName"required onchange=checkfirstname()>
+    <div id="fname"></div>
    
   </div>
         </div><!--col-md-4-->
@@ -23,7 +24,7 @@
         <div class="col-md-4"> <!--This col for alerts -->
         <!--This alert is for success -->
         <?php
-        $success =false;
+        $success =true;
         if($success){
           echo "<div class='info'>
           <div class='alert alert-success alert-dismissible fade show' role='alert'>
@@ -74,3 +75,23 @@
       </div><!--row-->
     </div>
 </div>
+
+<script>
+ function checkfirstname() {
+        let name = document.getElementById("firstname");
+        let name_value = name.value;
+        let regex = /^[a-zA-Z\s]+$/;
+        if (!regex.test(name_value)) {
+            // alert("Invalid name! Only letters and spaces are allowed.");
+            let fname = document.getElementById('fname').innerHTML =
+                "<p style='color:red'>Name only allow to enter letter or spaces</p>";
+            name.value = "";
+            
+        }
+      
+       if(regex==true){
+        let fname = document.getElementById('fname').innerHTML ="";
+                
+       }
+    }
+</script>
