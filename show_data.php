92de889484d34b9bf06fe85a9e016469c396943b
krishna soni <soni01krishna@gmail.com>
2023-03-24 22:57:33 +0530

    create show_data.php

--- show_data.php
+++ show_data.php
@@ -5,8 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All data</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <header>
+        <div class="nav">
+            <div class="conrainer">
+                <h1 id="all-detail">All detail</h1>
+            </div>
+        </div>
+</header>
+    <div class="container">
+
+    </div><!-- container-->
 </body>
 </html>
