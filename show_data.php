84bc30c8f30c2264a498bbfa7b2a3071e49f04ca
[vivek kumar] <[vivekmisha140@gmail.com]>
2023-03-25 12:27:12 +0530

    save a static value of qualification

--- show_data.php
+++ show_data.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All data</title>
     <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
 </head>
+
 <body>
     <header class="header">
         <div class="nav justify-content-center ">
@@ -15,63 +18,81 @@
                 <h1 id="all-detail ">All detail</h1>
             </div>
         </div>
-       
-</header>
+
+    </header>
     <div class="container" id="main-page">
-    <div class="row">
-        <div class="col-md-4">
-            <strong>1</strong>
-        </div>
-        <div class="col-md-4">
-            <p>Name:</p>
-        </div>
-        <div class="col-md-4 d-flex justify-content-around">
-            <button type="button" class="btn btn-success">Edit</button>
-            <button type="button" class="btn btn-danger ">Delete</button>
-        </div>
-    </div><!--row-->
-    <hr>
-    <div class="row">
-        <div class="col-md-3">
-            <img src="https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80" class="img-fluid rounded-top" alt="">
-        </div>
-        <div class="col-md-9">
-            <div class="table-responsive">  
-                <table class="table table-primary">
-                   <tr>
-                    <th>Email:<th>
-                    <td>krishna@gmil.com</td>    
-                    </tr>   
-                    <tr>
-                    <th>Phone:<th>
-                    <td>959990099</td>    
-                    </tr>   
-                    <tr>
-                    <th>Designation:<th>
-                    <td>Fullstack Developer</td>    
-                    </tr>  
-                   
-                    <tr class="text-center">
-                        <th>Qualification</th>
-                    </tr>
-                    <tr>
-                    <th>
-                        10th
-                    </th>
-                    
-                        
-                    </tr>
-
-                    
-
-                </table>
+        <div class="row">
+            <div class="col-md-4">
+                <strong>1</strong>
             </div>
-            
+            <div class="col-md-4">
+                <p>Name:</p>
+            </div>
+            <div class="col-md-4 d-flex justify-content-around">
+                <button type="button" class="btn btn-success">Edit</button>
+                <button type="button" class="btn btn-danger ">Delete</button>
+            </div>
         </div>
-    </div>
-        
-</div><!-- main-page-->
+        <!--row-->
+        <hr>
+        <div class="row">
+            <div class="col-md-3">
+                <img src="https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80"
+                    class="img-fluid rounded-top" alt="">
+            </div>
+            <div class="col-md-9">
+                <div class="table-responsive">
+                    <table class="table table-primary">
+                        <tr>
+                            <th>Email:
+                            <th>
+                            <td>krishna@gmil.com</td>
+                        </tr>
+                        <tr>
+                            <th>Phone:
+                            <th>
+                            <td>959990099</td>
+                        </tr>
+                        <tr>
+                            <th>Designation:
+                            <th>
+                            <td>Fullstack Developer</td>
+                        </tr>
+
+                        <tr class="text-center">
+                            <th>Qualification</th>
+                        </tr>
+                        <tr>
+                            <th>
+                                class:
+                            </th>
+                            <td> 10</td>
+
+                        </tr>
+                        <tr>
+                            <th>Board</th>
+                            <td>Board name</td>
+                        </tr>
+
+                        <tr>
+                            <th>
+                            Percentage
+
+                            </th>
+                            <td>98</td>
+                        </tr>
+
+                    </table>
+                </div>
+
+            </div>
+        </div>
+
+    </div><!-- main-page-->
     </div><!-- container-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
